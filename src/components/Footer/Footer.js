30194ef8c5e08980d5ade455ccb40e0dc02eabb6
Cristian Viterales <cviterales@cooperativabatan.com.ar>
2021-03-06 16:48:56 -0300

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook,
   faGithub,
-  faInstagram,
   faLinkedin,
-  faTwitter,
-  faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import style from './style.module.scss'
-import { faEnvelope, faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 
 const Footer = () => {
@@ -18,13 +14,13 @@ const Footer = () => {
       <div className={style.border}></div>
       <div className={style.footer_submenu}>
         <div className={style.submenu_social}>
-          <a href="https://www.linkedin.com/in/cristian-viterales" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/cristian-viterales" target="_blank"  aria-label="LinkedIn" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} size="lg" />
           </a >
-          <a href="https://github.com/cviterales" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/cviterales" target="_blank"  aria-label="GitHub" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} size="lg" />
           </a>
-          <a href="mailto: cviterales@gmail.com" target="_blank" rel="noopener noreferrer">
+          <a href="mailto: cviterales@gmail.com" target="_blank"  aria-label="Gmail" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faEnvelope} size="lg" />
           </a>
         </div>
