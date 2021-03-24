89264e6536af33779a81c8c779e50c375aba8784
Cristian Viterales <cviterales@cooperativabatan.com.ar>
2021-03-24 12:52:09 -0300

    refactorized MoviesDetails component and style Sass - BEM

--- src/components/Movies/MovieDetails/MovieDetails.js
+++ src/components/Movies/MovieDetails/MovieDetails.js
@@ -1,70 +1,87 @@
 import React, { useState } from "react";
 import "./style.scss";
 import StarsRating from "../StarsRating/StarsRating";
-
 import moment from "moment";
+
+const MovieInfo = ({movie}) => {
+  return (
+    <div className="info">
+      <p>{movie.adult ? "+18" : "+13"}</p>
+      <p>|</p>
+      <p>{movie.original_language.toUpperCase()}</p>
+      <p>|</p>
+      <div className="info__star">
+        <StarsRating rating={movie.vote_average} />
+      </div>
+    </div>
+  );
+};
+
+const MovieDescription = ({movie}) => {
+  return (
+    <div className="details">
+      <div className="details__first">
+        <p>Votes: {movie.vote_average}</p>
+        <p>Amount Votes: {movie.vote_count}</p>
+      </div>
+      <div className="details__second">
+        <p>{movie.overview}</p>
+      </div>
+
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <p>Release: {moment(movie.release_date).format("DD-MM-YYYY")}</p>
+      </div>
+    </div>
+  );
+};
 
 
 const MovieDetails = (props) => {
-  const [movie] = useState(props.location.state ? props.location.state.movie : null);
+  const [movie] = useState(
+    props.location.state ? props.location.state.movie : null
+  );
   const [loaded, setLoaded] = useState(false);
 
-  let renderDetails = <div style={{fontSize: "20px", color: "#fff", position: "absolute", left: "50%", top: "50%"}}>OH! An error ocurred!</div>
+  let renderDetails = (
+    <div
+      style={{
+        fontSize: "20px",
+        color: "#fff",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+      }}
+    >
+      OH! An error ocurred!
+    </div>
+  );
 
   if (movie) {
     renderDetails = (
       <div className="content">
-      <div className="content__image">
         {loaded && (
           <>
-            <div className="content__image__background">
-              <div className="content__image__background__details">
+            <div className="content__background">
+              <div className="content__background__details">
                 <img
-                  className="content__image__background__details__img"
+                  className="content__background__details__img"
                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                   alt={movie.title}
                 />
               </div>
-              <div className="content__image__background__movie">
+              <div className="content__background__movie">
                 <p>
-                  {movie.title ? movie.title : movie.original_name} ({moment(movie.release_date).format("YYYY")})
+                  {movie.title ? movie.title : movie.original_name} (
+                  {moment(movie.release_date).format("YYYY")})
                 </p>
-                <div className="content__image__background__movie__info">
-                  <p>
-                    {movie.adult ? "+18" : "+13"}
-                  </p>
-                  <p>|</p>
-                  <p>
-                    {movie.original_language.toUpperCase()}
-                  </p>
-                  <p>|</p>
-                  <div className="content__image__background__movie__info__star">
-                    <StarsRating rating={movie.vote_average} />
-                  </div>
-                </div>
-                <div className="content__image__background__movie__details">
-                  <div className="content__image__background__movie__details__first">
-                    <p>Votes: {movie.vote_average}</p>
-                    <p>Amount Votes: {movie.vote_count}</p>
-                  </div>
-                  <div
-                    className="content__image__background__movie__details__second"
-                  >
-                    <p>{movie.overview}</p>
-                  </div>
-
-                  <div style={{ textAlign: "center", marginTop: "20px" }}>
-                    <p>
-                      Release: {moment(movie.release_date).format("DD-MM-YYYY")}
-                    </p>
-                  </div>
-                </div>
+                <MovieInfo movie={movie}/>
+                <MovieDescription movie={movie} />
               </div>
             </div>
           </>
         )}
         <img
-          className="content__image__img"
+          className="content__img"
           src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
           alt={movie.title}
           onLoad={() => {
@@ -72,13 +89,9 @@ const MovieDetails = (props) => {
           }}
         />
       </div>
-{/*       {loaded && <div className={style.content_movie_details}></div>} */}
-    </div>
-    )
+    );
   }
-  return (
-    <div>{renderDetails}</div>
-  );
+  return <div>{renderDetails}</div>;
 };
 
 export default MovieDetails;
